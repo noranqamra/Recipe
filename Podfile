68b631e8526bdf6887e09df02e9e669fc265f930
NoranQamra <noranamra@gmail.com>
2021-08-22 16:46:25 +0200

    adding initial project files

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Recipe Search Demo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Recipe Search Demo
+#pod 'Alamofire', '~> 5.4'
+  target 'Recipe Search DemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Recipe Search DemoUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ Recipe Search DemoTests/Recipe_Search_DemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Recipe_Search_DemoTests.swift
+//  Recipe Search DemoTests
+//
+//  Created by Nouran Alaa on 22/08/2021.
+//
+
+import XCTest
+@testable import Recipe_Search_Demo
+
+class Recipe_Search_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
